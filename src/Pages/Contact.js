abd48b1964b1ed3fd088f1e3762e8f425b49cee2
Justin Mozley <jtmozley@gmail.com>
2019-07-06 22:06:48 -0500

    brought react portfolio into repo for deployment

---
+++ src/Pages/Contact.js
@@ -0,0 +1,34 @@
+// import React, { Component } from "react";
+// import TempNav from "../Components/TempNav";
+// import Grid from "@material-ui/core/Grid";
+// import { Container } from "@material-ui/core";
+
+// class Portfolio extends Component {
+//   state = {
+    
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <TempNav />
+//         <Container maxWidth="md">
+//           <Grid container spacing={4}>
+//             {this.state.Projects.map(project => (
+//               <Grid item xs={6} align="center">
+//                 <ProjectCard
+//                   title={project.title}
+//                   description={project.description}
+//                   image_url={project.image_url}
+//                   site_url={project.site_url}
+//                   repo_url={project.repo_url}
+//                 />
+//               </Grid>
+//             ))}
+//           </Grid>
+//         </Container>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Portfolio;
